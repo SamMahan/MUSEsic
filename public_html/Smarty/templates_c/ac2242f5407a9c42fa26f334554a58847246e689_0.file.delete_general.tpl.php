5cacfc8b9a3b0c0f8046afdf388410d10f406b42
Donovan <dv1187@messiah.edu>
2017-11-02 19:25:48 -0400

    push

--- public_html/Smarty/templates_c/ac2242f5407a9c42fa26f334554a58847246e689_0.file.delete_general.tpl.php
+++ public_html/Smarty/templates_c/ac2242f5407a9c42fa26f334554a58847246e689_0.file.delete_general.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2017-11-01 18:53:35
+<?php /* Smarty version 3.1.27, created on 2017-11-02 19:19:00
          compiled from "/Applications/MAMP/htdocs/MUSEsic/public_html/templates/modals/delete_general.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:161462526659fa506fe48821_48908939%%*/
+/*%%SmartyHeaderCode:181045561859fba7e46a6206_74428143%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,20 +9,20 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'ac2242f5407a9c42fa26f334554a58847246e689' => 
     array (
       0 => '/Applications/MAMP/htdocs/MUSEsic/public_html/templates/modals/delete_general.tpl',
-      1 => 1509576715,
+      1 => 1509664720,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '161462526659fa506fe48821_48908939',
+  'nocache_hash' => '181045561859fba7e46a6206_74428143',
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_59fa506fe848b1_51070560',
+  'unifunc' => 'content_59fba7e4778437_28183569',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_59fa506fe848b1_51070560')) {
-function content_59fa506fe848b1_51070560 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_59fba7e4778437_28183569')) {
+function content_59fba7e4778437_28183569 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '161462526659fa506fe48821_48908939';
+$_smarty_tpl->properties['nocache_hash'] = '181045561859fba7e46a6206_74428143';
 ?>
 <div class="modal" id="delete_general">
     <div class="modal-dialog">
@@ -36,8 +36,8 @@ $_smarty_tpl->properties['nocache_hash'] = '161462526659fa506fe48821_48908939';
             </div>
                 <div class="modal-footer">
 
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-success">Save Changes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
 
                 </div>
         </div>
