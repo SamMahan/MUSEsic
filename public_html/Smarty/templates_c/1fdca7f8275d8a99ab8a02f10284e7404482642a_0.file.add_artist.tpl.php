e08ea09b138f17fea437b1076b14e9f31a13a65e
SamMahan <SM1520@messiah.edu>
2017-11-02 21:02:58 -0400

    user js updates

--- public_html/Smarty/templates_c/1fdca7f8275d8a99ab8a02f10284e7404482642a_0.file.add_artist.tpl.php
+++ public_html/Smarty/templates_c/1fdca7f8275d8a99ab8a02f10284e7404482642a_0.file.add_artist.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2017-11-02 20:38:57
+<?php /* Smarty version 3.1.27, created on 2017-11-02 20:56:39
          compiled from "C:\MAMP\htdocs\MUSEsic\public_html\templates\modals\add_artist.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:124425359759fbbaa1dc49b1_08422551%%*/
+/*%%SmartyHeaderCode:101755157359fbbec76ff134_53392910%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,22 +9,29 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '1fdca7f8275d8a99ab8a02f10284e7404482642a' => 
     array (
       0 => 'C:\\MAMP\\htdocs\\MUSEsic\\public_html\\templates\\modals\\add_artist.tpl',
-      1 => 1509664879,
+      1 => 1509670047,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '124425359759fbbaa1dc49b1_08422551',
+  'nocache_hash' => '101755157359fbbec76ff134_53392910',
+  'variables' => 
+  array (
+    'modalopen' => 0,
+    'successArray' => 0,
+    'populateArray' => 0,
+  ),
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_59fbbaa1dcd256_77411920',
+  'unifunc' => 'content_59fbbec776a921_76003550',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_59fbbaa1dcd256_77411920')) {
-function content_59fbbaa1dcd256_77411920 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_59fbbec776a921_76003550')) {
+function content_59fbbec776a921_76003550 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '124425359759fbbaa1dc49b1_08422551';
+$_smarty_tpl->properties['nocache_hash'] = '101755157359fbbec76ff134_53392910';
 ?>
-<div class="modal fade" id="add_artist" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="add_artist" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" <?php echo $_smarty_tpl->tpl_vars['modalopen']->value;?>
+>
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -35,54 +42,63 @@ $_smarty_tpl->properties['nocache_hash'] = '124425359759fbbaa1dc49b1_08422551';
             </div>
             <div class="modal-body">
 
-                <form class="form-horizontal">
+                <form class="form-horizontal" method="post" action="<?php echo @constant('WEB_PATH');?>
+artist_list.php">
                     <fieldset>
 
                         <!-- Text input-->
-                        <div class="form-group">
+                        <div class="form-group <?php echo $_smarty_tpl->tpl_vars['successArray']->value[0];?>
+" >
                             <label class="col-md-4 control-label" for="artist_name">Artist Name:</label>
                             <div class="col-md-4">
-                                <input id="artist_name" name="artist_name" type="text" placeholder="name" class="form-control input-md">
+                                <input id="artist_name" name="artist_name" type="text" placeholder="name" class="form-control input-md" <?php echo $_smarty_tpl->tpl_vars['populateArray']->value[0];?>
+>
 
                             </div>
                         </div>
 
                         <!-- Text input-->
-                        <div class="form-group">
+                        <div class="form-group <?php echo $_smarty_tpl->tpl_vars['successArray']->value[1];?>
+" >
                             <label class="col-md-4 control-label" for="popular_song">Most Popular Song:</label>
                             <div class="col-md-4">
-                                <input id="popular_song" name="popular_song" type="text" placeholder="song name" class="form-control input-md">
+                                <input id="popular_song" name="popular_song" type="text" placeholder="song name" class="form-control input-md" <?php echo $_smarty_tpl->tpl_vars['populateArray']->value[1];?>
+>
 
                             </div>
                         </div>
 
                         <!-- Text input-->
-                        <div class="form-group">
+                        <div class="form-group <?php echo $_smarty_tpl->tpl_vars['successArray']->value[2];?>
+" >
                             <label class="col-md-4 control-label" for="number_songs">Number of Songs:</label>
                             <div class="col-md-4">
-                                <input id="number_songs" name="number_songs" type="text" placeholder="number" class="form-control input-md">
+                                <input id="number_songs" name="number_songs" type="text" placeholder="number" class="form-control input-md" <?php echo $_smarty_tpl->tpl_vars['populateArray']->value[2];?>
+>
 
                             </div>
                         </div>
 
                         <!-- Text input-->
-                        <div class="form-group">
+                        <div class="form-group <?php echo $_smarty_tpl->tpl_vars['successArray']->value[3];?>
+" >
                             <label class="col-md-4 control-label" for="number_albums">Number of Albums:</label>
                             <div class="col-md-4">
-                                <input id="number_albums" name="number_albums" type="text" placeholder="number" class="form-control input-md">
+                                <input id="number_albums" name="number_albums" type="text" placeholder="number" class="form-control input-md" <?php echo $_smarty_tpl->tpl_vars['populateArray']->value[3];?>
+>
 
                             </div>
+                        </div>
+
+                        <div class="modal-footer">
+
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-success">Save Changes</button>
+
                         </div>
 
                     </fieldset>
                 </form>
-
-            </div>
-            <div class="modal-footer">
-
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-success">Save Changes</button>
-
             </div>
         </div>
     </div>
