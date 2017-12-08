47921ca960ec68d6d47e39b753f5100803b88c29
Donovan <dv1187@messiah.edu>
2017-12-08 09:11:30 -0500

    add delete functions to buttons and it should work

--- public_html/assets/js/album/delete_album.js
+++ public_html/assets/js/album/delete_album.js
@@ -1,44 +1,27 @@
 $(document).ready(function(){
     $("#delete-album-button").click(function() {
-        var id = this.attr("data-albumid");
+
+        var song_id2 = $(this).data("albumid");
+        var info = {
+            song_id: song_id2
+        };
 
         $.ajax({
             url: "../../../controllers/api/album/get_songs.php",
             type: "post",
-            data: id,
-            dataType: 'json',
+            data: info,
             success: function(response) {
-                $('#album-song-response').prepend(response);
+                if (response === 0) {
+                    alert ("Could not delete" + response + ".");
+                } else if (response === 1) {
+                    alert ("Please sign in to access this function.")
+                } else {
+                    alert("Successfully deleted " + response + ".");
+                }
             },
             error: function() {
                 alert("Your request could not be processed at this time.")
             }
-        });
-
-        $("#confirm-delete-album").click(function() {
-
-            $.ajax({
-                url: "../../../controllers/api/album/delete_album.php",
-                type: "post",
-                data: id,
-                dataType: 'json',
-                success: function(response) {
-                    if (response === 0) {
-                        alert ("Could not delete" + response + ".");
-                    } else if (response === 1) {
-                        alert ("Please sign in to access this function.")
-                    } else {
-                        alert("Successfully deleted " + response + ".");
-                    }
-                },
-                error: function() {
-                    alert("Your request could not be processed at this time.")
-                }
-            });
-        });
-        $("#cancel").click(function() {
-
-
         });
     });
 });
--- public_html/assets/js/song/delete_song.js
+++ public_html/assets/js/song/delete_song.js
@@ -1,17 +1,15 @@
-
-
 $(document).ready(function() {
     $(".delete-song-button").click(function() {
 
-
         var song_id2 = $(this).data("songid");
         var info = {
-            song_id: song_id2,
-        }
+            song_id: song_id2
+        };
+
         $.ajax({
             url: "../../../controllers/api/song/delete_song.php",
             type: "post",
-            data:info,
+            data: info,
             success: function(response) {
                 if (response === 0) {
                     alert ("Could not delete" + response + ".");
